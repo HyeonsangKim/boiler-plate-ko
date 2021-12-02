ed9501da618db09cab29c12ff8d64f6f0db34ad4
Kim HyeonSang <k98hh@naver.com>
2021-12-02 18:16:33 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodtParser = require('body-parser')
+
+const config = require('./config/key')
+const {User} = require("./models/User")
+
+//application/x-www-form-urlencoded
+app.use(bodtParser.urlencoded({extended:true}))
+
+//application/json
+app.use(bodtParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://johnahn:abcd1234@boilerplate.xefbm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=> console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then(()=> console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안뇽')
+  res.send('Hello World! 안뇽하세요')
+})
+
+app.post('/register',(req,res)=>{
+  // 회원 가입 할때 필요한 정보들을 client 에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  user.save((err,userInfo)=>{
+    if(err) return res.json({succcess:false,err})
+    return res.status(200).json({
+      succcess:true
+    })
+  })
+
 })
 
 app.listen(port, () => {
